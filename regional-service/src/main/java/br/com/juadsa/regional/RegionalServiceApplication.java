31d5b4ffa75cbea2928fea3295ae12a99e9770da
Michael Barroso <michael_p._b.15600@hotmail.com>
2025-11-06 22:51:10 -0300

    style: Alteração do nome da classe de applicação do regional-service.

--- regional-service/src/main/java/br/com/juadsa/regional/RegionalServiceApplication.java
+++ regional-service/src/main/java/br/com/juadsa/regional/RegionalServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GerenciamentoJuadsaAppApplication {
+public class RegionalServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GerenciamentoJuadsaAppApplication.class, args);
+		SpringApplication.run(RegionalServiceApplication.class, args);
 	}
 
 }
